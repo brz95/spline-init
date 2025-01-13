3665e99b2cdfbd6f679d7d38516376a94d63c16f
Deni Mamakaev <abuizidov@bk.ru>
2025-01-13 11:33:36 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://storage.yandexcloud.net/wb-ads-sample/scene.splinecode" />
+  );
+}
